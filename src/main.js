3cea50b7ad7513bc00f3279e8d7f5e02a02a5e82
mrmiguetara <miguelag1999@hotmail.com>
2021-11-03 17:31:59 -0400

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueBarcodeScanner from 'vue-barcode-scanner'
 
 Vue.config.productionTip = false
+Vue.use(VueBarcodeScanner)
 
 new Vue({
   render: h => h(App),
